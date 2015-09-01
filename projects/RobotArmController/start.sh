a587018a745f9e2491a0119c6771198ff408080b
Nobuhiko Miyamoto <TyouKyozyakuTaisitu@yahoo.co.jp>
2015-09-01 12:18:56 +0900

    1218

--- projects/RobotArmController/start.sh
+++ projects/RobotArmController/start.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 cd `dirname $0`
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../DLL
-cdir=$(cd $(dirname $0);pwd)
+cdir=${PWD}
 python startNamingService.py
+workspaceDir=../../workspace
+if [ ! -e $workspaceDir ]; then
+	mkdir $workspaceDir
+fi
 sh rtcStart_exe.sh
-cd ../../workspace
+cd $workspaceDir
 ../Manager/Cpp/rtcd_p/rtcd_p -f ../projects/RobotArmController/Cpp/rtc.conf&
 python ../Manager/Python/rtcd_p.py -f ../projects/RobotArmController/Python/rtc.conf&
 cd ${cdir}
